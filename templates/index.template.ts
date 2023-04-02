5f2b25d5a325d3469c2cf37e8318057565780a95
Gabriele Napoli <gabriele.nap@gmail.com>
2023-04-02 20:29:44 +0200

    first commit

---
+++ templates/index.template.ts
@@ -0,0 +1,19 @@
+export const blocks = [
+  {
+    id: 1,
+    type: "heroSection",
+    data: {
+      title: "Titolo primo blocco",
+      description: "Descrizione primo blocco",
+    },
+    enable: true,
+  },
+  {
+    id: 2,
+    type: "workWithUs",
+  },
+  {
+    id: 3,
+    type: "workWithUs",
+  },
+];
